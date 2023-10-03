eb683366398ac325ac1b6f9773f21d85eb1eef11
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-10-03 15:32:53 -0700

    My Commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for value in range(0, 100):
-    print("{:02}".format(value), end=", ")
-    if value == 99:
+    if value < 99:
+        print("{:02}".format(value), end=", ")
+    else:    
         print("{:02}".format(value))
