ba7459df27a81ec87d5f666e24d07897f880ada8
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-10-05 14:09:37 -0700

    My Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,12 @@
 if __name__ == "__main__":
     import sys
     arg_length = len(sys.argv)
-    if arg_length - 1 >= 1:
+    if arg_length - 1 > 1:
         print("{} arguments:".format(arg_length - 1))
+        for position in range(1, arg_length):
+            print("{}: {}".format(position, str(sys.argv[position])))
+    elif arg_length - 1 == 1:
+        print("{} argument:".format(arg_length - 1))
         for position in range(1, arg_length):
             print("{}: {}".format(position, str(sys.argv[position])))
     else:
