990a861977de4a4f481b47d35e518232662097d3
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-10-05 14:01:13 -0700

    My Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_length = len(sys.argv)
+    if arg_length - 1 >= 1:
+        print("{} arguments:".format(arg_length - 1))
+        for position in range(1, arg_length):
+            print("{}: {}".format(position, str(sys.argv[position])))
+    else:
+        print("{} arguments.".format(arg_length - 1))
