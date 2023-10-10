f60844370c9f476c46a97a7b1aabaf6fd284b63e
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-10-10 11:32:02 -0700

    My Commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[0] = None
-        tup_len = 0, sentence[0]
+        tup_len = len(sentence), sentence[0]
         return tup_len
     tup_len = len(sentence), sentence[0]
     return tup_len
