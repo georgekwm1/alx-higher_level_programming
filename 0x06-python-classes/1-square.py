7eec2b427048f3b624a62a237788d76680dfc9d4
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-10-25 18:17:24 +0100

    Commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Square Class Definition'''
 class Square:
+    '''Private instance attribute: size'''
     def __init__(self, size):
         '''Private instance attribute: size'''
         self.__size = size
