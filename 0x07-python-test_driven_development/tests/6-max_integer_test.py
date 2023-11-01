9fe4b6c4817aadf74a1b95e1faa1aa1074371b16
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-11-01 08:08:51 +0100

    My Commit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,3 +8,6 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([]), None)
+    def test_values(self):    
+        self.assertRaises(TypeError, max_integer, "Hello")
