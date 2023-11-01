9cea6d99c5c5f871fe684ae06d5a556a4479164f
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-11-01 06:57:21 +0100

    My Commit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,6 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
