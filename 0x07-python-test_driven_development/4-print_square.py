0f4937ac168e50d68be6a306f3465b4a2c061dc4
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-11-01 09:39:04 +0100

    Push

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if not size:
-        raise ValueError("Input an Integer")
+
     for i in range(size):
         print("{}".format(symbol * size), end='\n')
