069f10364de6c1fadd4f7a79ebdd5dc8ecc7c173
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-11-02 00:59:09 +0100

    Commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Definition of a text indentation function"""
 
 def text_indentation(text):
+    """
+    This function indents a text.
+
+    Args:
+    a (str): Text to be indented.
+
+    Returns:
+    Prints indented Text
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for char in text:
