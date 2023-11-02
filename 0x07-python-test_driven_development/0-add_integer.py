34851ea941ac1a71d9f7097c172793336c881498
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-11-02 01:09:24 +0100

    My Commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 """Definition of the addition function"""
 
 def add_integer(a, b=98):
-    
+
     """
     This function adds two integers.
 
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
     int: Sum of the integers.
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
