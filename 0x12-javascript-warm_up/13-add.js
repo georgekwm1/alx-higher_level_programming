4fb974a56bc760241b78d80121a0bc0990eaf36a
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-08 16:19:18 -0700

    My Commit

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-exports.add = (a, b) => {
-  return (a + b);
-};
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const myNumber = parseInt(process.argv[2]);
-
-if (myNumber) {
-  for (let i = 0; i < myNumber; i++) {
-    console.log('C is fun');
-  }
-} else console.log('Missing number of occurrences');
