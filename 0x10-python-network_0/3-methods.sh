8718b6df40414ecfd0521f8ccee7260a1fc16562
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-26 14:19:45 -0700

    My Commit

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Lists the HTTP methods a server accepts
-curl -X OPTIONS -I "$1" | grep -i '^Allow:'
+curl -s -X OPTIONS -i "$1" | awk '/Allow:/ {print substr($0, index($0, ":") + 2)}'
