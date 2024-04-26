983ec8169970a931c423197b90b368d2117b027d
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-26 14:47:42 -0700

    My Commit

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to a URL passed as an argument, and displays only the status code of the response
-echo "$(curl -s -w '%{http_code}' -o /dev/null "$1")"
+echo -n "$(curl -s -w '%{http_code}' -o /dev/null "$1")"
