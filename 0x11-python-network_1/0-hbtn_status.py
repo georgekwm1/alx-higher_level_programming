c6c4ab712cf32e7baa9efcb9b883152ece21b9c2
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-28 14:44:15 -0700

    My Commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
-    print("\t- content:", body.decode('utf-8'))
+    print("\t- utf8 content:", body.decode('utf-8'))
